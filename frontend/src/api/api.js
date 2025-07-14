f07a44700f7377c958b73ee233e01a15027a18f8
minikavya11 <kavyasreeperka45@gmail.com>
2025-07-14 12:25:44 +0530

    backend url updated

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://leaderboard-backend-ol9f.onrender.com", // backend running here
+  baseURL: "https://leaderboard-backend-ol9f.onrender.com/api", // backend running here
 });
 
 export default API;
